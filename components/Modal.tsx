96e230d4f26f2b347d627b53ce802369c1c20b1a
Adeyemi <addegbenga@gmail.com>
2022-03-03 21:18:59 +0100

    added passhref

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -36,7 +36,7 @@ const MyModal: NextPage<props> = ({ isOpen, setIsOpen }) => {
       <Transition appear show={isOpen} as={Fragment}>
         <Dialog
           as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
+          className="fixed inset-0 z-60 overflow-y-auto"
           onClose={closeModal}
         >
           <div className="min-h-screen px-4 text-center">
